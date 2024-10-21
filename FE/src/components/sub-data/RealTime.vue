e603cc311f50e6d6d3efc7e1a9faaf42613bccec
어지민 <djwlalssla@naver.com>
2024-10-21 16:54:12 +0900

    [FE][feat]: 서브 데이터 블록 크기 수정

---
+++ FE/src/components/sub-data/RealTime.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="real-time-con con shadow"></div>
+</template>
+
+<style scoped>
+.real-time-con {
+  width: 800px;
+  height: 100%;
+
+  /* background-color: red; */
+}
+</style>
