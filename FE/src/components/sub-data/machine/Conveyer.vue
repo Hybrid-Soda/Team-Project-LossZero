e273d06b8115a675fd72c84856652dd17b0cff51
어지민 <djwlalssla@naver.com>
2024-10-24 17:13:34 +0900

    [FE][design]: 스마트 팩토리 동작 버튼 추가

--- FE/src/components/sub-data/machine/Conveyer.vue
+++ FE/src/components/sub-data/machine/Conveyer.vue
@@ -13,21 +13,28 @@ function onOff() {
     <!-- 작동중일 때는 gif, 그렇지 않을 때는 이미지 출력 -->
     <img src="@/assets/img/gif/belt.gif" alt="카메라" v-if="isOperate" />
     <img src="@/assets/img/belt.jpg" alt="카메라" v-else />
-    
+
     <!-- 작동 상태 표시 -->
-    <span class="is-operate"> {{ isOperate ? "벨트 동작중" : "벨트 정지" }} </span>
-    
+    <span class="is-operate">
+      {{ isOperate ? "벨트 동작중" : "벨트 정지" }}
+    </span>
+
     <!-- 버튼 클릭 시 이미지와 상태 변경 -->
-    <button @click="onOff" :class="{ 'inactive': !isOperate }">
-      <img src="@/assets/img/Activating.svg" alt="Activating" :style="{ filter: isOperate ? 'none' : 'grayscale(100%)' }" />
+
+    <button @click="onOff" :class="{ inactive: !isOperate }">
+      <img
+        src="@/assets/img/Activating.svg"
+        alt="Activating"
+        :style="{ filter: isOperate ? 'none' : 'grayscale(100%)' }"
+      />
     </button>
   </div>
 </template>
 
 <style scoped>
 img {
-  width: 150px;
-  height: 150px;
+  width: 130px;
+  height: 130px;
 }
 
 .is-operate {
@@ -47,7 +54,7 @@ button img {
 
 /* 정지 상태일 때 버튼 아이콘의 색상을 변경 (회색) */
 button.inactive img {
-  color: #D9D9D9; /* 텍스트 색상을 바꾸고 싶을 경우 */
+  color: #d9d9d9;
 }
 
 /* 일반 버튼 상태 */
