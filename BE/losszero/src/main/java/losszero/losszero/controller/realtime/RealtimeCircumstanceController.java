2e85983664ae9fd0ae3eef2053b1c65d63e65363
임경태 <limkt0925@gmail.com>
2024-10-29 23:03:48 +0900

    [BE][refactor] : realtime controller 리팩토링

--- BE/losszero/src/main/java/losszero/losszero/controller/realtime/RealtimeCircumstanceController.java
+++ BE/losszero/src/main/java/losszero/losszero/controller/realtime/RealtimeCircumstanceController.java
@@ -2,35 +2,34 @@ package losszero.losszero.controller.realtime;
 
 import losszero.losszero.dto.realtime.RealtimeCircumstanceDTO;
 import losszero.losszero.service.realtime.RealtimeCircumstanceService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import lombok.RequiredArgsConstructor;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/realtime/circumstance")
 public class RealtimeCircumstanceController {
 
-    @Autowired
-    private RealtimeCircumstanceService realtimeCircumstanceService;
+    private final RealtimeCircumstanceService realtimeCircumstanceService;
 
     @PostMapping
-    public ResponseEntity<?> saveCircumstanceData(@RequestParam Integer lineId, @RequestBody RealtimeCircumstanceDTO data) {
-        if (lineId == null) {
-            return ResponseEntity.badRequest().body(Map.of("message", "lineId는 필수 파라미터입니다."));
-        }
+    public ResponseEntity<Map<String, String>> saveCircumstanceData(
+            @RequestParam int lineId, @RequestBody RealtimeCircumstanceDTO data) {
+
         realtimeCircumstanceService.saveCircumstanceData(lineId, data);
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "데이터 전송에 성공했습니다."));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Map.of("message", "데이터 전송에 성공했습니다."));
     }
 
     @GetMapping
-    public SseEmitter streamRealtimeData(@RequestParam Integer lineId) {
-        if (lineId == null) {
-            throw new IllegalArgumentException("lineId는 필수 파라미터입니다.");
-        }
+    public SseEmitter streamRealtimeData(@RequestParam int lineId) {
+
         SseEmitter emitter = new SseEmitter();
         realtimeCircumstanceService.streamRealtimeData(lineId, emitter);
         return emitter;
--- BE/losszero/src/main/java/losszero/losszero/controller/realtime/RealtimeProductController.java
+++ BE/losszero/src/main/java/losszero/losszero/controller/realtime/RealtimeProductController.java
@@ -2,35 +2,34 @@ package losszero.losszero.controller.realtime;
 
 import losszero.losszero.dto.realtime.RealtimeProdDTO;
 import losszero.losszero.service.realtime.RealtimeProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import lombok.RequiredArgsConstructor;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/realtime/prod")
 public class RealtimeProductController {
 
-    @Autowired
-    private RealtimeProductService realtimeProductService;
+    private final RealtimeProductService realtimeProductService;
 
     @PostMapping
-    public ResponseEntity<?> saveProductData(@RequestParam Integer lineId, @RequestBody RealtimeProdDTO productData) {
-        if (lineId == null) {
-            return ResponseEntity.badRequest().body(Map.of("message", "lineId는 필수 파라미터입니다."));
-        }
+    public ResponseEntity<?> saveProductData(
+            @RequestParam int lineId, @RequestBody RealtimeProdDTO productData) {
+
         realtimeProductService.saveProductData(lineId, productData);
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "데이터 전송에 성공했습니다."));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Map.of("message", "데이터 전송에 성공했습니다."));
     }
 
     @GetMapping
-    public SseEmitter streamRealtimeData(@RequestParam Integer lineId) {
-        if (lineId == null) {
-            throw new IllegalArgumentException("lineId는 필수 파라미터입니다.");
-        }
+    public SseEmitter streamRealtimeData(@RequestParam int lineId) {
+
         SseEmitter emitter = new SseEmitter();
         realtimeProductService.streamRealtimeData(lineId, emitter);
         return emitter;
