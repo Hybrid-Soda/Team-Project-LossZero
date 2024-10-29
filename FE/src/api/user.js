6f7d914e0fb2cb3879226440cc199e553c7de7a6
어지민 <djwlalssla@naver.com>
2024-10-29 09:58:59 +0900

    [FE][feat]: API 연결 초기 설정

---
+++ FE/src/api/user.js
@@ -0,0 +1,11 @@
+import { localAxios } from "@/utils/request";
+
+const axios = localAxios();
+
+export function login(userInfo) {
+  return axios({
+    url: `/login`,
+    method: "post",
+    userInfo,
+  });
+}
