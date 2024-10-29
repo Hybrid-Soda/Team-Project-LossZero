691431fc31aad801bdba8eea7230e9c00abeb5d5
rt21hyuk <rt21hyuk@gmail.com>
2024-10-29 16:31:34 +0900

    [EM] [feat]: 기구학 제어 코드

---
+++ EM/ARM/robot_arm_arduino/main/ServoControl.h
@@ -0,0 +1,30 @@
+#ifndef SERVO_CONTROL_H
+#define SERVO_CONTROL_H
+
+#include <Servo.h>
+
+// 모드 정의
+enum Mode {
+  CONVEYOR,
+  STANDBY,
+  RECYCLE,
+  DEFECTIVE,
+  WAYPOINT
+};
+
+// 서보 모터 초기화 함수
+void initializeServos();
+
+// 서보 모터 부드럽게 이동 함수
+void moveServoSmoothly(Servo &servo, int &currentAngle, int targetAngle, const char* axisName);
+
+// 특정 모드로 이동하는 함수
+void setModeAngles(Mode mode);
+
+// 대기 모드로 이동하는 함수
+void moveToStandby();
+
+// 시퀀스 실행 함수
+void executeSequence(int sequenceType);
+
+#endif
