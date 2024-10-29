5d91fb719b56ff75b203a2a699499688e73a32df
임경태 <limkt0925@gmail.com>
2024-10-29 22:39:15 +0900

    [BE][refactor] : realtimeProd entity 리팩토링

--- BE/losszero/src/main/java/losszero/losszero/entity/realtime/RealtimeProd.java
+++ BE/losszero/src/main/java/losszero/losszero/entity/realtime/RealtimeProd.java
@@ -1,11 +1,16 @@
 package losszero.losszero.entity.realtime;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "realtime_prod")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RealtimeProd {
 
     @Id
@@ -26,57 +31,4 @@ public class RealtimeProd {
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
-
-    // 기본 생성자
-    public RealtimeProd() {}
-
-    // Getter and Setter
-
-    public int getRealtimeProdId() {
-        return realtimeProdId;
-    }
-
-    public void setRealtimeProdId(int realtimeProdId) {
-        this.realtimeProdId = realtimeProdId;
-    }
-
-    public Integer getLineId() {
-        return lineId;
-    }
-
-    public void setLineId(Integer lineId) {
-        this.lineId = lineId;
-    }
-
-    public int getNormal() {
-        return normal;
-    }
-
-    public void setNormal(int normal) {
-        this.normal = normal;
-    }
-
-    public int getDefective() {
-        return defective;
-    }
-
-    public void setDefective(int defective) {
-        this.defective = defective;
-    }
-
-    public int getReusable() {
-        return reusable;
-    }
-
-    public void setReusable(int reusable) {
-        this.reusable = reusable;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
