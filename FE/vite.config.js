a14b18029f3d648806c8e9f4bcbb2f9fa2bb3131
김구태 <gootae0220@naver.com>
2024-10-30 13:24:37 +0900

    [INFRA] vite.config.js 수정

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // 외부 네트워크에서 접근 가능하게 설정
-    port: 5173      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
+    port: 5173,      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
+    hmr: {
+      protocol: 'wss', // HMR을 WebSocket Secure로 설정
+      host: 'k11e202.p.ssafy.io', // HMR의 호스트를 프론트엔드 서버와 일치하도록 설정
+      port: 443 // HTTPS 포트
+    },
+    https: true, // HTTPS 사용
   }
 })
