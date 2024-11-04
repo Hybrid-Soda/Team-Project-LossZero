df1ec85cc142b40ef1436cb893510b35c8c4494d
김구태 <gootae0220@naver.com>
2024-11-04 19:22:39 +0900

    [INFRA] fix : nginx 설저, hmr 설정 수정

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -15,7 +15,13 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // 외부 네트워크에서 접근 가능하게 설정
-    port: 5173      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
+    port: 5173,      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
     
+    hmr: {
+      protocol: 'wss',
+      host: 'k11e202.p.ssafy.io', // 배포된 서버의 호스트 이름
+      clientPort: 443             // HTTPS의 기본 포트로 설정
+    }
+
   }
 })
