dc25ce5f5be3516c5c2836a9045c1cc396f7a959
임경태 <limkt0925@gmail.com>
2024-11-04 10:19:25 +0900

    [BE][feat] : url 변경

--- BE/losszero/src/main/java/losszero/losszero/config/MqttConfig.java
+++ BE/losszero/src/main/java/losszero/losszero/config/MqttConfig.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class MqttConfig {
 
     private static final String[] topics = {"realtime-oper", "realtime-prod", "realtime-circ"};
-    private static final String brokerUrl = "tcp://k11e202.p.ssafy.io:1883";
+    private static final String brokerUrl = "tcp://localhost:1883";
 //    private static final String brokerUrl = "tcp://localhost:1883";
     private static final String clientId = "SpringBoot-MQTT-Client-" + UUID.randomUUID();
 
