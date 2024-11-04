036a69eacaf91ed3195e1287e49de17da0e52d88
김구태 <gootae0220@naver.com>
2024-11-04 19:06:35 +0900

    [INFRA] fix: websocket 관련 vite.config.js 수정

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -15,7 +15,13 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // 외부 네트워크에서 접근 가능하게 설정
-    port: 5173      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
+    port: 5173,      // 원하는 포트 (Docker의 포트 매핑과 일치해야 함)
+    
+    hmr: {
+      protocol: 'wss',          // 보안을 위해 WebSocket Secure 사용
+      host: 'k11e202.p.ssafy.io', // 배포된 서버의 호스트 이름
+      port: 9001                 // WebSocket에 연결할 포트
+    }
     // hmr: {
     //   protocol: 'wss', // HMR을 WebSocket Secure로 설정
     //   host: 'k11e202.p.ssafy.io', // HMR의 호스트를 프론트엔드 서버와 일치하도록 설정
