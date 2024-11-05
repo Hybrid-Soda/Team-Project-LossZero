ecdcbed4cbe1ef022516ffdc6bbc08d228a3c809
임경태 <limkt0925@gmail.com>
2024-11-05 11:16:10 +0900

    [BE][refactor] : realtime refactoring

--- BE/losszero/src/main/java/losszero/losszero/service/realtime/RealtimeProductService.java
+++ BE/losszero/src/main/java/losszero/losszero/service/realtime/RealtimeProductService.java
@@ -1,5 +1,6 @@
 package losszero.losszero.service.realtime;
 
+import lombok.RequiredArgsConstructor;
 import losszero.losszero.dto.realtime.RealtimeProductDTO;
 import losszero.losszero.entity.date.DateProd;
 import losszero.losszero.entity.realtime.RealtimeProd;
@@ -7,58 +8,61 @@ import losszero.losszero.repository.date.DateProdRepository;
 import losszero.losszero.repository.realtime.RealtimeProductRepository;
 
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class RealtimeProductService {
 
-    @Autowired
-    private RealtimeProductRepository realtimeProductRepository;
-
-    @Autowired
-    private DateProdRepository dateProductRepository;
+    private final RealtimeProductRepository realtimeProductRepository;
+    private final DateProdRepository dateProductRepository;
 
     public void saveProductData(RealtimeProductDTO productData) {
+        saveRealtimeProduct(productData);
+        updateDateProduct(productData);
+    }
+
+    private void saveRealtimeProduct(RealtimeProductDTO productData) {
+        RealtimeProd realtimeProd = RealtimeProd.builder()
+                .lineId(productData.getLineId())
+                .normal(productData.getQuality().getNormal())
+                .defective(productData.getQuality().getDefective())
+                .reusable(productData.getQuality().getReusable())
+                .createdAt(LocalDateTime.now())
+                .build();
+        realtimeProductRepository.save(realtimeProd);
+    }
+
+    private void updateDateProduct(RealtimeProductDTO productData) {
         int lineId = productData.getLineId();
         int normal = productData.getQuality().getNormal();
         int defective = productData.getQuality().getDefective();
         int reusable = productData.getQuality().getReusable();
 
-        // 실시간 데이터를 realtime_prod 테이블에 저장
-        RealtimeProd realtimeProd = new RealtimeProd();
-        realtimeProd.setLineId(lineId);
-        realtimeProd.setNormal(normal);
-        realtimeProd.setDefective(defective);
-        realtimeProd.setReusable(reusable);
-        realtimeProd.setCreatedAt(LocalDateTime.now());
-        realtimeProductRepository.save(realtimeProd);
-
-        // 현재 날짜를 기반으로 date_prod에 누적 데이터를 저장
         LocalDate currentDate = LocalDate.now();
-        Optional<DateProd> optionalDateProd = dateProductRepository.findByLineIdAndDate(lineId, currentDate);
+        DateProd dateProd = dateProductRepository.findByLineIdAndDate(lineId, currentDate)
+                .map(existingProd -> updateExistingDateProd(existingProd, normal, defective, reusable))
+                .orElseGet(() -> createNewDateProd(lineId, currentDate, normal, defective, reusable));
 
-        DateProd dateProd;
-        if (optionalDateProd.isPresent()) {
-            // 기존 레코드가 있으면 누적 업데이트
-            dateProd = optionalDateProd.get();
-            dateProd.setSumNormal(dateProd.getSumNormal() + normal);
-            dateProd.setSumDefective(dateProd.getSumDefective() + defective);
-            dateProd.setSumReusable(dateProd.getSumReusable() + reusable);
-        } else {
-            // 없으면 새로운 레코드를 생성
-            dateProd = new DateProd();
-            dateProd.setLineId(lineId);
-            dateProd.setDate(currentDate);
-            dateProd.setSumNormal(normal);
-            dateProd.setSumDefective(defective);
-            dateProd.setSumReusable(reusable);
-        }
-
-        // 변경 사항 저장
         dateProductRepository.save(dateProd);
+    }
+
+    private DateProd updateExistingDateProd(DateProd dateProd, int normal, int defective, int reusable) {
+        dateProd.setSumNormal(dateProd.getSumNormal() + normal);
+        dateProd.setSumDefective(dateProd.getSumDefective() + defective);
+        dateProd.setSumReusable(dateProd.getSumReusable() + reusable);
+        return dateProd;
+    }
+
+    private DateProd createNewDateProd(int lineId, LocalDate date, int normal, int defective, int reusable) {
+        return DateProd.builder()
+                .lineId(lineId)
+                .date(date)
+                .sumNormal(normal)
+                .sumDefective(defective)
+                .sumReusable(reusable)
+                .build();
     }
 }
