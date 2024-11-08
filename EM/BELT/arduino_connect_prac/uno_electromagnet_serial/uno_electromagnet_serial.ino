568daf9941f7ac6df02d82c53db6b709dbca1c99
강현성 <tjd4025@naver.com>
2024-11-08 17:44:53 +0900

    [EM][feat] :PI_ LED 연결

--- EM/BELT/arduino_connect_prac/uno_electromagnet_serial/uno_electromagnet_serial.ino
+++ EM/BELT/arduino_connect_prac/uno_electromagnet_serial/uno_electromagnet_serial.ino
@@ -1,28 +1,49 @@
-int Electromagnet = 2;
+// Arduino A Code
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(Electromagnet, OUTPUT); 
+  Serial.begin(9600); // 디버깅용 시리얼
+  mySerial.begin(9600); // 아두이노 간 통신용 시리얼
+  Serial.println("Arduino A ready.");
+  delay(5000); //5초 대기 후 시작
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sendCommand("Command1");
+  waitForCompletion();
+  delay(2000); // 2초 대기 후 다시 시작
 
-  if(Serial.available()){
-    char c = Serial.read();
-    if (c=='0'){
-      int Electromagnet = 2;
-      Serial.println("전자석 on");
-      digitalWrite(Electromagnet, HIGH);
-    }
-    else{
-      Serial.println("전자석 off");
-      digitalWrite(Electromagnet, LOW);
+  sendCommand("Command2");
+  waitForCompletion();
+  delay(2000); // 2초 대기 후 다시 시작
+
+  sendCommand("Command3");
+  waitForCompletion();
+  delay(2000); // 2초 대기 후 다시 시작
+
+  sendCommand("Command4");
+  waitForCompletion();
+  delay(2000); // 2초 대기 후 다시 시작
+}
+
+void sendCommand(String command) {
+  mySerial.println(command);
+  Serial.println("Sent: " + command);
+}
+
+void waitForCompletion() {
+  while (true) {
+    if (mySerial.available()) {
+      String message = mySerial.readStringUntil('\n');
+      message.trim();
+      Serial.println("Received: " + message);
+      if (message.endsWith("Complete")) {
+        break; // 완료 메시지 수신 시 루프 탈출
+      }
     }
   }
-
-  
-
-  delay(1000);
 }
