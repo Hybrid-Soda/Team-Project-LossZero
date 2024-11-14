6ded9ffa33c4acd5d8059b04bab0678040efd531
어지민 <djwlalssla@naver.com>
2024-11-14 15:17:24 +0900

    [FE] fix: 실시간 정보 수정

--- FE/src/stores/operate.js
+++ FE/src/stores/operate.js
@@ -1,58 +1,68 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export const useOperateStore = defineStore("operate", () => {
-  const machineOnOff = ref(false);
-  const coveyerOnOff = ref(false);
-  const armOnOff = ref(false);
-  const cameraOnOff = ref(false);
+export const useOperateStore = defineStore(
+  "operate",
+  () => {
+    const machineOnOff = ref(false);
+    const coveyerOnOff = ref(false);
+    const armOnOff = ref(false);
+    const cameraOnOff = ref(false);
 
-  function machineOnOffFun() {
-    machineOnOff.value = !machineOnOff.value;
-    console.log(machineOnOff.value);
-  }
+    function machineOnOffFun() {
+      machineOnOff.value = !machineOnOff.value;
+      console.log(machineOnOff.value);
+    }
 
-  function armOn() {
-    armOnOff.value = true;
-    // console.log(armOnOff.value);
-  }
+    function armOn() {
+      if (machineOnOff.value) {
+        armOnOff.value = true;
+      }
+      // console.log(armOnOff.value);
+    }
 
-  function armOff() {
-    armOnOff.value = false;
-    // console.log(armOnOff.value);
-  }
+    function armOff() {
+      armOnOff.value = false;
+      // console.log(armOnOff.value);
+    }
 
-  function coveyerOn() {
-    coveyerOnOff.value = true;
-    // console.log(coveyerOnOff.value);
-  }
+    function coveyerOn() {
+      if (machineOnOff.value) {
+        coveyerOnOff.value = true;
+      }
+      // console.log(coveyerOnOff.value);
+    }
 
-  function coveyerOff() {
-    coveyerOnOff.value = false;
-    // console.log(coveyerOnOff.value);
-  }
+    function coveyerOff() {
+      coveyerOnOff.value = false;
+      // console.log(coveyerOnOff.value);
+    }
 
-  function cameraOn() {
-    cameraOnOff.value = true;
-    // console.log(cameraOnOff.value);
-  }
+    function cameraOn() {
+      if (machineOnOff.value) {
+        cameraOnOff.value = true;
+      }
+      // console.log(cameraOnOff.value);
+    }
 
-  function cameraOff() {
-    cameraOnOff.value = false;
-    // console.log(cameraOnOff.value);
-  }
+    function cameraOff() {
+      cameraOnOff.value = false;
+      // console.log(cameraOnOff.value);
+    }
 
-  return {
-    armOnOff,
-    coveyerOnOff,
-    cameraOnOff,
-    machineOnOff,
-    machineOnOffFun,
-    armOn,
-    armOff,
-    coveyerOn,
-    coveyerOff,
-    cameraOn,
-    cameraOff,
-  };
-});
+    return {
+      armOnOff,
+      coveyerOnOff,
+      cameraOnOff,
+      machineOnOff,
+      machineOnOffFun,
+      armOn,
+      armOff,
+      coveyerOn,
+      coveyerOff,
+      cameraOn,
+      cameraOff,
+    };
+  },
+  { persist: true }
+);
