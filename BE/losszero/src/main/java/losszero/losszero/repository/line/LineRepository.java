38bc82e6e7b4c34aee5c4ec1d05ab53d9382925c
김구태 <gootae0220@naver.com>
2024-11-19 09:51:44 +0900

    [INFRA] chore : 파일 구성 변경

--- BE/losszero/src/main/java/losszero/losszero/repository/line/LineRepository.java
+++
@@ -1,7 +0,0 @@
-package losszero.losszero.repository.line;
-
-import losszero.losszero.entity.line.Line;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LineRepository extends JpaRepository<Line, Integer> {
-}
--- BE/losszero/src/test/java/losszero/losszero/repository/LineRepository.java
+++
@@ -1,7 +0,0 @@
-package losszero.losszero.repository;
-
-import losszero.losszero.entity.line.Line;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LineRepository extends JpaRepository<Line, Integer> {
-}
